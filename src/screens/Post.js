6e8ae6d409d490b0b6514c59f85063d61843bf85
ashbar409 <ashbar409@gmail.com>
2022-08-31 17:01:15 -0400

    Adding front end CRUD

--- src/screens/Post.js
+++ src/screens/Post.js
@@ -1,14 +1,39 @@
 import {useParams} from "react-router-dom"
 import { useEffect, useState } from "react";
-import {getBlogById} from "../services/constants"
+import {getBlogById, createComment, updateComment, deleteComment, getCommentsByBlogId} from "../services/constants"
+import { handlers } from "browser-router/html5-history/adapter";
 
 //choose the correct post via id
 function Post() {
 
   const { post_id } = useParams();
+  console.log(post_id)
   //extracting id from url
 
   const [post, setPost] = useState([{}])
+  const [comments, setComments] = useState([{comment: ""}])
+  const [newComment, setNewComment] = useState("")
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setNewComment(e.target.value);
+  }
+
+  const handleClickUpdate = (e, content, id) => {
+    e.preventDefault()
+    updateComment(content, id)
+  }
+
+  const handleClickDelete = (e, id) => {
+    e.preventDefault()
+    deleteComment(id)
+  }
+
+  const handleClickCreate = (e, content, blogId) => {
+    e.preventDefault()
+    console.log(post_id)
+    createComment(content, blogId)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +41,18 @@ function Post() {
         const response = await getBlogById(post_id)
         console.log(response)
         setPost(response)
+      } catch (error) {
+          console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await getCommentsByBlogId(post_id)
+        setComments(response)
       } catch (error) {
           console.log(error)
       }
@@ -32,6 +69,23 @@ function Post() {
       <div className="content">
         <h3>{post.title}</h3>
         <p>{post.body}</p>
+      </div>
+      <div className="comment-section">
+        {
+          comments.map(comment => {
+            return <div className="comment">
+              <p>{comment.comment}</p>
+              <input type="text" defaultValue="Update comment" onChange={((e) => handleChange(e))}/>
+              <button onClick={(e) => handleClickUpdate(e, newComment, comment._id)}>Update</button>
+              <div className="update"></div>
+              <button onClick={(e) => handleClickDelete(e, comment._id)}>Delete</button>
+            </div>
+          })
+        }
+        <div>
+          <input type="text" defaultValue="Type a comment" onChange={(e) => handleChange(e)}/>
+          <button onClick={(e) => handleClickCreate(e, newComment, post_id)}>Post comment</button>
+        </div>
       </div>
     </div>
   )
--- src/services/constants.js
+++ src/services/constants.js
@@ -10,3 +10,27 @@ export async function getBlogById(id) {
     console.log(result)
     return result
 }
+
+export async function getCommentsByBlogId(blogId){
+    const result = await (await axios.get(`https://bloggr-project.herokuapp.com/comments/`, {blog: blogId})).data.comments
+    console.log(result)
+    return result
+}
+
+export async function createComment(bodyText, blogId) {
+    const result = await axios.post(`https://bloggr-project.herokuapp.com/comments/`, {comment: bodyText, blog: blogId})
+    console.log("Created comment")
+    console.log(result)
+}
+
+export async function updateComment(bodyText, id) {
+    const result = await axios.put(`https://bloggr-project.herokuapp.com/comments/${id}`, {comment: bodyText})
+    console.log("Updated comment")
+    return result
+}
+
+export async function deleteComment(id) {
+    const result = await axios.delete(`https://bloggr-project.herokuapp.com/comments/${id}`, )
+    console.log("Deleted comment")
+    return result
+}
