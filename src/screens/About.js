935f10826111d464dbe40c23c14f830323340839
ashbar409 <ashbar409@gmail.com>
2022-08-31 10:22:35 -0400

    Finisnhing touches added

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,7 +2,7 @@ function About() {
     return (
       <div className="about">
         <div className="post">
-          <h2>Bloggr was my final project at Per Scholas as a student. It's a full stack application using the MERN stack of MongoDB, Express, React, and Node.js. I plan to use this website to track my journey in software engineering.</h2>
+          <h2>Bloggr was my final project at Per Scholas as a student. It's a full stack application using the MERN stack of MongoDB, Express, React, and Node.js. I plan to use this website to track my journey in software engineering. Starting from my humble beginnings 8 years ago where I knew nothing to now at the end of my 15 weeks of Per Scholas, I plan to document these and anything that comes in the future</h2>
         </div>
       </div>
     );
